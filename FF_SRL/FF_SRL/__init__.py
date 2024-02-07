1d809687a53077777061b116466ed31941e345f8
mnaskret <mnaskret@gmail.com>
2024-02-07 23:14:12 +0100

    Revert "Uploading simulation code with an example"
    This reverts commit f8f92c12c6876ecfa7b8a43e24ad8f016cc79fb9.
    
    	deleted:    FF_SRL/FF_SRL.egg-info/PKG-INFO
    	deleted:    FF_SRL/FF_SRL.egg-info/SOURCES.txt
    	deleted:    FF_SRL/FF_SRL.egg-info/dependency_links.txt
    	deleted:    FF_SRL/FF_SRL.egg-info/requires.txt
    	deleted:    FF_SRL/FF_SRL.egg-info/top_level.txt
    	deleted:    FF_SRL/FF_SRL/__init__.py
    	deleted:    FF_SRL/FF_SRL/__pycache__/__init__.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/__init__.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/bvh.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/bvh.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/integrator.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/integrator.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/laparoscope.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/laparoscope.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/model.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/model.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/modelDO.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/modelDO.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/render.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/render.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/utils.cpython-38.pyc
    	deleted:    FF_SRL/FF_SRL/__pycache__/utils.cpython-39.pyc
    	deleted:    FF_SRL/FF_SRL/bvh.py
    	deleted:    FF_SRL/FF_SRL/integrator.py
    	deleted:    FF_SRL/FF_SRL/laparoscope.py
    	deleted:    FF_SRL/FF_SRL/model.py
    	deleted:    FF_SRL/FF_SRL/modelDO.py
    	deleted:    FF_SRL/FF_SRL/render.py
    	deleted:    FF_SRL/FF_SRL/scenes/liverRetraction.npz
    	deleted:    FF_SRL/FF_SRL/scenes/liverRetraction.usd
    	deleted:    FF_SRL/FF_SRL/tests/testLiverRetraction.py
    	deleted:    FF_SRL/FF_SRL/utils.py
    	deleted:    FF_SRL/setup.py

--- FF_SRL/FF_SRL/__init__.py
+++
@@ -1,7 +0,0 @@
-from FF_SRL.model import SimObject, SimMesh, SimLockBox, SimConnector, SimElasticRod, SimContext, SimInteractions, SimModel, SimEnvironment
-from FF_SRL.modelDO import SimMeshDO, SimConnectorDO, SimLockBoxDO, SimModelDO, SimEnvironmentDO, SimRigidDO
-from FF_SRL.integrator import SimIntegrator, SimIntegratorDO
-from FF_SRL.utils import launchTransformVecArrayWarp, transformVecArray, getBoxSpanVectors, checkIfWithinBounds, launchRemapAToB, launchRemapAToBLimited, generateTriMeshCapsule, generateTriMeshSphere, multiplyListVector, subtractListVectors, addListVectors, calculateListVectorNormalized, crossListVector, getTransformationMatrix, launchTransformVecArrayWarp2
-from FF_SRL.render import WarpRaycastRendererDO
-from FF_SRL.laparoscope import SimLaparoscopeDO
-from FF_SRL.bvh import SimBVH
